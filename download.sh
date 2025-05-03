cffdcc03489df8e790da1c66e3f1a3ce619247ae
Wesley Yan S. B. <102262759+simplyYan@users.noreply.github.com>
2025-05-03 21:55:28 +0000

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p DYSORDER
+cd DYSORDER
+
+declare -A files=(
+  ["LowNet.sh"]="https://raw.githubusercontent.com/simplyYan/DYSORDER/refs/heads/main/tools/LowNet/LowNet.sh"
+  ["mrchainsaw.sh"]="https://raw.githubusercontent.com/simplyYan/DYSORDER/refs/heads/main/tools/MrChainsaw/mrchainsaw.sh"
+  ["MrSpecter.sh"]="https://raw.githubusercontent.com/simplyYan/DYSORDER/refs/heads/main/tools/MrSpecter/MrSpecter.sh"
+)
+
+# Baixa cada arquivo
+for filename in "${!files[@]}"; do
+  echo "Downloading $filename..."
+  curl -sSL "${files[$filename]}" -o "$filename"
+  chmod +x "$filename"
+  sudo cp "$filename" /usr/local/bin/
+done
+
+echo "All DYSØRDER tools have been downloaded and globalized. All ready to use, in any directory :)"
